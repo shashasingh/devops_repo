4f90f1c22f9122e92f42aa782d6bbcbc6e292c6e
Ubuntu <ubuntu@ip-172-31-42-86.ap-northeast-3.compute.internal>
2023-03-27 09:56:24 +0000

    Initial commit AVI

---
+++ hellowordapp/src/main/java/com/gitassignment/App.java
@@ -0,0 +1,15 @@
+package com.gitassignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String git ="Welcome to Devops"; 
+    	
+        System.out.println( "Hello Git!!"+git );
+    }
+}
