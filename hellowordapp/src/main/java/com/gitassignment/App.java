5cb1d4c8475dd81bc56a1162e7f299eabd6411bf
Ubuntu <ubuntu@ip-172-31-42-86.ap-northeast-3.compute.internal>
2023-04-08 08:03:10 +0000

    April_8 commitAvi

--- hellowordapp/src/main/java/com/gitassignment/App.java
+++ hellowordapp/src/main/java/com/gitassignment/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	String git ="Welcome to Devops"; 
+        System.out.println( "Hello Git_April_8!!"+git );
     	
         System.out.println( "Hello Git!!"+git );
     }
