f371b217f538434f335af54de55c6cf47c705cb9
Lenovo <shasha070singh@gmail.com>
2023-04-15 19:41:17 +0530

    App.java

--- hellowordapp/src/main/java/com/gitassignment/App.java
+++ hellowordapp/src/main/java/com/gitassignment/App.java
@@ -1,5 +1,9 @@
 package com.gitassignment;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 /**
  * Hello world!
  *
