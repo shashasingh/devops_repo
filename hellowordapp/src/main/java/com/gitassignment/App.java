c92b2c7516a45ed138a82e7a6616fbf7e6ef55ee
shashasingh <116787138+shashasingh@users.noreply.github.com>
2023-04-15 16:53:48 +0530

    War changes

--- hellowordapp/src/main/java/com/gitassignment/App.java
+++ hellowordapp/src/main/java/com/gitassignment/App.java
@@ -4,13 +4,18 @@ package com.gitassignment;
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+public class App extends SpringBootServletInitializer
 {
-    public static void main( String[] args )
-    {
-    	String git ="Welcome to Devops"; 
-        System.out.println( "Hello Git_April_8!!"+git );
-    	
-        System.out.println( "Hello Git!!"+git );
-    }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(App.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
 }
+
