f6e81d560a523ee6464029e7a9cc301490f4b99c
shashasingh <116787138+shashasingh@users.noreply.github.com>
2023-04-15 16:54:42 +0530

    Update App.java

--- hellowordapp/src/main/java/com/gitassignment/App.java
+++ hellowordapp/src/main/java/com/gitassignment/App.java
@@ -8,11 +8,6 @@ package com.gitassignment;
 public class App extends SpringBootServletInitializer
 {
 
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(App.class);
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
